67a4aefdfc94b657f39b1c7988411901181f5f96
christian <rodriguezchristian2005@gmail.com>
2025-07-19 23:43:39 -0230

    cofres actualización
    nada

--- scripts/CofreConfig.cs
+++ scripts/CofreConfig.cs
@@ -1,8 +1,9 @@
 using Godot;
 using System;
 
-public partial class CofreConfig : Node
+public partial class CofreConfig : Resource
 {
 	[Export] public SpriteFrames Animaciones;
+	[Export] public AnimationPlayer _anim;
 	
 }
